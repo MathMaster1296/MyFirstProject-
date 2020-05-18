78c0415be55e0b8a17e5e85625a0e5c5f303e0fd
MathMaster1296 <prathammukewar@outlook.com>
2020-05-18 02:02:08 +0000

    Being boring

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-var = 1 + 1 + 1
-print("The answer is ", str(var))
+str1 = "Hello world"
+print(str1.count("o"))
+
+
+
+
+
+
+
+number1 = int(input("Enter number1 "))
+number2 = int(input("Enter number2 "))
+
+print(f'Result of {number1} + {number2} = {number1 + number2}')
+print(f'Result of {number1} - {number2} = {number1 - number2}')
+print(f'Result of {number1} * {number2} = {number1 * number2}')
+print(f'Result of {number1} / {number2} = {number1 / number2}')
+
+# % means modulus (Reminder)
+print(f'Result of {number1} % {number2} = {number1 % number2}')
+
+# ** means Exponent
+print(f'Result of {number1} ** {number2} = {number1 ** number2}')
+
+# // means integer division
+print(f'Result of {number1} // {number2} = {number1 // number2}')
+
+
+
+
+
+
+
+first_name = input("What is your first name? ")
+last_name = input("What is you last name? ")
+year_of_birth = int(input("What is your year of birth? "))
+address = input("What is your address? ")
+zip_code = input("What is your zip code? ")
+age = 2020 - year_of_birth
+print(f"""Name: {first_name} {last_name}
+Age: {age}
+address: {address}
+zip code: {zip_code}""")
