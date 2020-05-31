7b0d97045b2326d5b3f6585642728f58458dc9c5
null null <prathammukewar@outlook.com>
2020-05-31 23:58:39 +0000

    Class #3

--- main.py
+++ main.py
@@ -1,42 +1,75 @@
-str1 = "Hello world"
-print(str1.count("o"))
+#Class #3
+done = "\nDone with Program. Proceeding to Next Program.\n\n\n"
+b = "nothing"
+c = "nothing"
+while not(b == "quit"):
+  b = input("Do you want to turn the fan on, off, or quit?: ")
+  if b == "on" or b == "off":
+    if (b == c):
+      print(f"Fan is already {b}")
+    else: 
+      print(f"Fan turned {b}")
+  elif not(b == "quit"):
+    print("Invalid Input. Please Try Again.")
+  c = b
+print(done)
 
 
 
+num = int(input("Enter a Number: "))
+if num % 2 == 1:
+  print("The number is odd.")
+else:
+  print("The number is even.")
+print(done)
 
 
 
-
-number1 = int(input("Enter number1 "))
-number2 = int(input("Enter number2 "))
-
-print(f'Result of {number1} + {number2} = {number1 + number2}')
-print(f'Result of {number1} - {number2} = {number1 - number2}')
-print(f'Result of {number1} * {number2} = {number1 * number2}')
-print(f'Result of {number1} / {number2} = {number1 / number2}')
-
-# % means modulus (Reminder)
-print(f'Result of {number1} % {number2} = {number1 % number2}')
-
-# ** means Exponent
-print(f'Result of {number1} ** {number2} = {number1 ** number2}')
-
-# // means integer division
-print(f'Result of {number1} // {number2} = {number1 // number2}')
+z = int(input("Enter a number: "))
+for a in range(z + 1):
+  print(a)
+print(done)
 
 
+text = input("What text do you want? ")
+number = 1
+for number1 in text:
+  print(f"{number} -> {number1}")
+  number += 1
+print(done)
 
 
+weightunit = input("Weight Unit: ")
+weight = int(input("Weight: "))
+if weightunit.lower() == "kg":
+  print(f"about {weight*2.2//1} pounds")
+elif weightunit.lower() == "lb":
+  print(f"about {weight//2.2} kilograms")
+else:
+  print("Invalid")
+print(done)
 
 
+age = int(input("What is your child's age? "))
+if age <= 0:
+  print("not born")
+elif 0 < age <= 3:
+  print("toddler")
+elif 3 < age <= 12:
+  print("child")
+elif 12 < age <= 18:
+  print("teenager")
+else:
+  print("adult")
+print(done)
 
-first_name = input("What is your first name? ")
-last_name = input("What is you last name? ")
-year_of_birth = int(input("What is your year of birth? "))
-address = input("What is your address? ")
-zip_code = input("What is your zip code? ")
-age = 2020 - year_of_birth
-print(f"""Name: {first_name} {last_name}
-Age: {age}
-address: {address}
-zip code: {zip_code}""")
+
+x = int(input("First number: "))
+y = int(input("Second number: "))
+if x > y:
+  print(f"{x} - {y} = {x - y}")
+elif x < y:
+  print(f"{y} - {x} = {y - x}")
+else:
+  print("They are both equal")
+print("Done with all Programs")
